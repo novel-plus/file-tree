644875224589567736389ab75e5961aa8cb0b945
fifteenmania <ktthee1995@gmail.com>
2021-11-08 15:14:18 +0900

    feat: renderer tree complete

--- file-tree/renderer.js
+++ file-tree/renderer.js
@@ -1,7 +1,37 @@
-function renderDirectoryTree(tree) {
-
+function renderDirectoryTreeRec(treeData) {
+    const dirNode = document.createElement('li');
+    dirNode.innerText = treeData.name;
+    if (treeData.items === []) return dirList;
+    const subList = document.createElement('ul');
+    for (const treeDataItem of treeData.items) {
+        if (treeDataItem.items === null) {
+            const subNode = document.createElement('li');
+            subNode.innerText = treeDataItem.name;
+            subList.appendChild(subNode);
+        } else {
+            const subTree = renderDirectoryTreeRec(treeDataItem);
+            subList.appendChild(subTree);
+        }
+    }
+    dirNode.appendChild(subList);
+    return dirNode;
 }
 
-const directoryTreeRootElement = document.getElementById('dir-tree-root')
+function renderDirectoryTree(treeData) {
+    const rootList = document.createElement('ul');
+    const tree = renderDirectoryTreeRec(treeData);
+    rootList.appendChild(tree);
+    return rootList;
+}
 
+const directoryTreeRootElement = document.getElementById('dir-tree-root');
+const openDirectoryElement = document.getElementById('open-directory-button');
+
+openDirectoryElement.addEventListener('click', () => {
+    fileApi.openDirectory().then((data) => {
+        if (data.canceled) return;
+        const tree = renderDirectoryTree(data.content);
+        directoryTreeRootElement.replaceChild(tree, directoryTreeRootElement.lastChild);
+    });
+})
 
--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const { opendir } = require('fs/promises')
 const path = require('path');
 
 const defaultEncoding = 'utf-8';
+
+class ErrorNotification {
+    constructor(msg) {
+        this.notification = new Notification("에러 알림", {body: msg})
+    }
+
+    show() {
+        this.notification.show();
+    }
+}
 
 async function getDirectoryTree(dirName, encoding=defaultEncoding) {
     const result = {
@@ -12,9 +22,9 @@ async function getDirectoryTree(dirName, encoding=defaultEncoding) {
     }
     const dir = await opendir(dirName, {encoding: encoding});
     for await (const dirent of dir) {
-        const direntPath = path.join(dirName, dirent.name)
+        const direntPath = path.join(dirName, dirent.name);
         if (dirent.isDirectory()) {
-            const subTree = await getDirectoryTree(direntPath)
+            const subTree = await getDirectoryTree(direntPath);
             result.items.push(subTree);
         } else {
             result.items.push({
@@ -27,6 +37,26 @@ async function getDirectoryTree(dirName, encoding=defaultEncoding) {
     return result;
 }
 
+async function handleOpenDirectory(targetWindow, basePath=app.getAppPath()) {
+    const fileSelect = await dialog.showOpenDialog(targetWindow, {
+        title: "폴더를 선택해주세요",
+        properties: ['openDirectory'],
+        defaultPath: basePath,
+        encoding: 'utf-8'
+    });
+    const result = {
+        canceled: fileSelect.canceled,
+        content: null
+    };
+    if (!fileSelect.canceled) {
+        result.content = await getDirectoryTree(fileSelect.filePaths[0]).catch((e) => {
+            result.canceled = true;
+            new ErrorNotification(e).show();
+        });
+    }
+    return result;
+}
+
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         webPreferences: {
@@ -34,17 +64,17 @@ function createMainWindow() {
         }
     });
     mainWindow.loadFile(path.join('file-tree', 'index.html'));
+    //mainWindow.webContents.openDevTools();
     return mainWindow;
-}
-
-function initialize() {
-    ipcMain.handle('file:openDirectory', () => {
-        
-    })
 }
 
 app.whenReady().then(()=>{
     createMainWindow();
+
+    ipcMain.handle('file:openDirectory', () => {
+        const focusedWindow = BrowserWindow.getFocusedWindow();
+        return handleOpenDirectory(focusedWindow);
+    })
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createMainWindow();
